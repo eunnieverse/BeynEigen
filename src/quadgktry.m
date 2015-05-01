854ce9640eaa3f0d5edb45ed3a554c832d5aeaf8
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-05-01 17:09:30 -0400

    added src and tex directories

---
+++ src/quadgktry.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% BeynEigen
+%%%%% Yoonkyung Eunnie Lee 
+%%%%% matlab program to find the eigenvalue of a nonlinear eigenproblem
+%%%%% using Beyn's contour integral method 
+%%%%% 2015.05.01
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+q=quadgk()
