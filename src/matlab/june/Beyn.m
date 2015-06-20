d4f1b8bd2d409f1a65f4c03bcefb7148f77f7d38
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-06-20 11:26:42 -0400

    Beyn.m updated to use nested quadrature

--- src/matlab/june/Beyn.m
+++ src/matlab/june/Beyn.m
@@ -1,5 +1,7 @@
-function [k, N, w_Beyn, i_Beyn, w_Beyn_err]=...
-        Beyn(k_guess,n,funA,fundA,N_in,w_Newt,i_Newt,w_err_cut)
+function [k, N, BeynA0, BeynA1, w_Beyn, i_Beyn, w_Beyn_err]=...
+        Beyn(k_in,N_in,BeynA0_in,BeynA1_in,...
+             n,funA,fundA,...
+             w_Newt,i_Newt,w_err_cut)
     %  function Beyn
     % inputs:  int k_guess: guess for number of eigenvalues
     %          int N_in: quadrature pts 
@@ -11,30 +13,47 @@ function [k, N, w_Beyn, i_Beyn, w_Beyn_err]=...
     %          cdouble w_Beyn[k]: list of eigenvalues found 
     %          cdouble w_Beyn_err[k]: list of corresponding error
     %          int i_Beyn[] list of corresponding eigenvalue index
-    % Yoonkyung Eunnie Lee 
-    % Last Updated 2015.06.18    
-
+    % Yoonkyung Eunnie Lee
+    % Last Updated 2015.06.18
+    
     %--- define contour 
-    if(mod(N_in,2)==0) N=N_in; else N=N_in+1; end %N is even. 
-    g0=0.0; rho=1.0; %circular contour 
-    [g, gp] = circcont(g0, rho, N); % construct contour N
-    %--- calculate dimension of the problem 
-    k_calc = cint(trace(trace(funA(wj)\fundA(wj)), g,gp);
-    l = max(k_guess,k_calc); 
-    M = rand(n,l);  % dimension of initial arbitrary mat. n x k 
+    q = ceil(log2(N_in)); %N should be 2^q 
+    N = 2^q; 
 
-    %--- compute BeynA0, BeynA1 for original function 
-    BeynA0 = getA(0,funA,M,g,gp); 
-    BeynA1 = getA(1,funA,g,gp);  
-    BeynA0_half = getA(0,funA,M,g(1:2:N),gp(1:2:N)); 
-    BeynA1_half = getA(1,funA,M,g(1:2:N),gp(1:2:N)); 
-    % %--- compute BeynA0, BeynA1 for reduced w_Newt 
-    % BeynA0 = getAmod(0,funA,w_Newt,M,g,gp);
-    % BeynA1 = getAmod(1,funA,w_Newt,M,g,gp); 
-    % BeynA0_half = getAmod(0,funA,w_Newt,M,g(1:2:N),gp(1:2:N)); 
-    % BeynA1_half = getAmod(1,funA,w_Newt,M,g(1:2:N),gp(1:2:N)); 
+    g0=0.0; rho=1.0;    %circular contour
+    [g, gp] = circcont_nest(g0, rho, N); % construct contour N
 
-    %--- Compute the SVD of BeynA0 
+    %--- determine the size guess for this problem 
+    if(N<8) 
+        funsize = @(z) trace(funA(z)\fundA(z));
+        k_calc = cint(funsize,g,gp); 
+        l = ceil(max(k_calc,k_in)); 
+    else
+        l = ceil(k_in+2);
+    end
+    M = rand(n,l);      % dimension of initial arbitrary mat. n x k 
+    
+    %--- compute BeynA0, BeynA1 & store BeynA0_in, BeynA1_in 
+    if(N>4)                  % if previous run data exist
+        BeynA0 = BeynA0_in*(N/2)*i; % sum from N/2 run, if they are given 
+        BeynA1 = BeynA1_in*(N/2)*i; % sum from N/2 run, if they are given
+        Nlist = N/2+1:N; 
+    else Nlist =1:N; end            % if this is the first run 
+    for j=Nlist
+        invAj = funA(g(j))\M;       
+        BeynA0 = BeynA0 + invAj * gp(j); 
+        BeynA1 = BeynA1 + invAj * g(j) * gp(j); 
+        if(j==N/2)% store the integral from N/2 run 
+            BeynA0_in = BeynA0/(N*i/2);  
+            BeynA1_in = BeynA1/(N*i/2); 
+        end
+    end
+    BeynA0 = BeynA0 /(N*i); 
+    BeynA1 = BeynA1 /(N*i);    
+    
+    % w_Beyn, i_Beyn, w_Beyn_err
+     
+    %--- Compute the SVD of BeynA0
     [V,Sigma,W] = svd(BeynA0); 
     Sigma = Sigma(1:l,1:l); % first l elements, diagonal matrix 
     s = diag(Sigma); % column
--- src/matlab/june/getA.m
+++ src/matlab/june/getA.m
@@ -1,11 +1,11 @@
 function [BeynA] = getA(p, funA, M, g, gp)
-   f_BeynA = @(z) (z^p)*funA(z)\M;
-   BeynA = cint(f_BeynA, g, gp);
-   
+    f_BeynA = @(z) (z^p)*funA(z)\M;
+    BeynA = cint(f_BeynA, g, gp);
+    
 function [BeynA] = getAmod(p, funA, w_Newt, M, g, gp)
     f_BeynA = @(z) (z^p)*fz(z,w_Newt)*funA(z)\M; 
     BeynA = cint(f_BeynA, g, gp);
-
+    
 function f = fz(z,w_Newt) %(z-w1)(z-w2)... 
     f=1; 
     if(length(w_Newt)>0)
@@ -20,4 +20,4 @@ function [] = BeynSVD(BeynA0,BeynA1)
     s = diag(Sigma); 
     k = sum(s>1e-15); %%actual rank 
     
-    V0 = V(1:n, 1:k); 
+    V0 = V(1:n, 1:k);
