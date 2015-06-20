f9fc2f50b3d339b2f2c78da8ace2751eb5791e1a
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-06-20 11:28:35 -0400

    added main.m to june folder

---
+++ src/matlab/june/main.m
@@ -0,0 +1,33 @@
+%function main()
+clear all; 
+close all; 
+
+%% problem definition 
+n=30;
+filebase = 'poly2_30'; 
+[coeffs, funA, fundA]=polydef(filebase,2,30); 
+%% starting values 
+N=4; 
+done=-1; %% indicator to stop simulation 
+w_Newt = []; 
+i_Newt = []; 
+k_guess=60; 
+%% limits
+w_err_cut = 1e-5; %% Beyn error cutoff 
+
+%% values that need to be stored outside: M, k, 
+while(done<0)
+    %% Beyn Step 
+    N_in = N; 
+    [k,N,w_Beyn,i_Beyn,w_Beyn_err]= Beyn(k_guess,n,funA,fundA,N_in, ...
+                                         w_Newt, i_Newt, w_err_cut); 
+    %% Newton Step 
+    [w_Newt,i_Newt]=Newton(w_Beyn,i_Beyn,funA,fundA); 
+    
+    if(length(w_Newt)==k)
+        N=2*N; 
+        k_guess = k; % update the guess for problem size. 
+    else
+        done=1; %yay!
+    end
+end 
