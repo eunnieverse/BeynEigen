da9983d260509d97a7c722482cb3a0aba3302a62
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-09-01 17:55:26 -0400

    added function dc that computes distance from a point to the analytical contour

--- src/matlab/aug/NestedContour.m
+++ src/matlab/aug/NestedContour.m
@@ -1,4 +1,4 @@
-function [gmax,dgmax,s,isinside]=NestedContour(g0,rho,Nmax) 
+function [gmax,dgmax,s,dc,isinside]=NestedContour(g0,rho,Nmax) 
     %- create a circular nested contour centered at g0, with radius rho 
     theta    = zeros(Nmax,1);
     theta(1) = 2*pi;  % qq = 0; 
@@ -9,11 +9,21 @@ function [gmax,dgmax,s,isinside]=NestedContour(g0,rho,Nmax)
     end
     gmax  = g0 + rho*( cos(theta) + 1i*sin(theta));
     dgmax =      rho*(-sin(theta) + 1i*cos(theta));
+    %----------------------------------------------------------------
     s = @(N) 2*pi*rho/N;        %- arc distance between adjacent pts
                                 %  as a function of N   
-                                
-    isinside = @subfun; 
-    function y=subfun(w)
-        y=find(abs(w-g0)<rho);
+    %-----------------------------------------------------------------                            
+    dc = @subfun1; 
+    function y=subfun1(w)
+        %- anaytical distance from w to the circular contour
+        y=zeros(size(w));
+        y=rho-abs(w-g0);
     end
+    %-----------------------------------------------------------------
+    isinside = @subfun2; 
+    function y=subfun2(w)
+        y=zeros(size(w)); 
+        y=abs(w-g0)<rho;
+    end
+    %-----------------------------------------------------------------                            
 end
