ec3f6683587e0e87b9f64c0015727f3c19f4a466
eunnie12 <eunnieverse@gmail.com>
2015-09-07 14:13:08 -0400

    adding julia version

---
+++ src/julia/funA.jl
@@ -0,0 +1,9 @@
+function funA(w)
+# funA, create a matrix-valued quadratic function from random matrices
+  	   n = 30;
+	   A0::Float64 = rand(n); 
+	   A1::Float64 = rand(n); 
+	   A1::Float64 = rand(n); 
+           A = w^2.*A2 + w.*A1 + A0;
+	   return A; 
+end
