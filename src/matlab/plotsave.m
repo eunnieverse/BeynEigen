c6fa7dab5f0efd10fea67d5891333abc40bbfc2b
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-09-15 13:51:17 -0400

    plotsave updated with eps -tiff

--- src/matlab/plotsave.m
+++ src/matlab/plotsave.m
@@ -1,5 +1,12 @@
 function plotsave(cfig, savefigbase, fignum, savejpg, saveeps)
-    savefigname=sprintf('%s_%03d',savefigbase,fignum); 
-    if(savejpg==1); saveas(cfig, strcat(savefigname,'.jpg'));       end; 
-    if(saveeps==1); saveas(cfig,strcat(savefigname,'.eps'),'epsc'); end;
+    set(cfig,'PaperPositionMode','auto'); 
+    set(findobj('type', 'text'), 'color', 'black');
+    set(findobj('type', 'line'), 'linewidth', 2.0);
+    savefigname=sprintf('%s_%03d',savefigbase,fignum);      
+    if(savejpg==1) 
+        saveas(cfig, strcat(savefigname,'.jpg'));       
+    end
+    if(saveeps==1)
+        print(cfig,strcat(savefigname,'.eps'),'-r600', '-depsc', '-tiff', '-cmyk'); 
+    end
 end
